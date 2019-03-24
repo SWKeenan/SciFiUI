1180282224f05fc85dbd2e35be9989781f4996f1
Shane Keenan <c17702215@mydit.ie>
2019-03-24 16:00:46 +0000

    Made all buttons clickable

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -38,16 +38,16 @@ public class UI extends PApplet
     {
         keyboard = new Computer(this, 0, 350, width, height);
         computer = new Computer(this, 100, 50, 600, 250);
-        b = new Button(this,  360, height-160, 100, 50, "B");
-        b2 = new Button(this, 465, height-160, 100,50, "C");
-        b3 = new Button(this, 260, height-50, 100, 50, "H");
-        b4 = new Button(this, 365, height-50, 100, 50, "I");
-        b5 = new Button(this, 470, height-50, 100, 50, "J");
-        b6 = new Button(this, 415, height-105, 100, 50, "F");
-        b7 = new Button(this, 520, height-105, 100, 50, "G");
-        b8 = new Button(this, 255, height-160, 100, 50, "A");
-        b9 = new Button(this, 205, height-105, 100, 50, "D");
-        b10 = new Button(this, 310, height-105, 100, 50, "E");
+        b = new Button(this,  360, height-200, 100, 50, "B");
+        b2 = new Button(this, 465, height-200, 100,50, "C");
+        b3 = new Button(this, 260, height-90, 100, 50, "H");
+        b4 = new Button(this, 365, height-90, 100, 50, "I");
+        b5 = new Button(this, 470, height-90, 100, 50, "J");
+        b6 = new Button(this, 415, height-145, 100, 50, "F");
+        b7 = new Button(this, 520, height-145, 100, 50, "G");
+        b8 = new Button(this, 255, height-200, 100, 50, "A");
+        b9 = new Button(this, 205, height-145, 100, 50, "D");
+        b10 = new Button(this, 310, height-145, 100, 50, "E");
         mc = new MovingCircle(this, width / 2, 175, 50);
         bar = new AnimatedBar(this, 20, 10, 20, 100);
         r = new Radar(this, 100, height-50, 100, 0.1f);
@@ -93,9 +93,63 @@ public class UI extends PApplet
         }
 
         if(mousePressed){
-            if(mouseX>360 && mouseX <460 && mouseY>height-160 && mouseY <height-110){
+            if(mouseX>360 && mouseX <460 && mouseY>height-200 && mouseY <height-150){
                 fill(255);
-                text("You pressed B", width/2, 175);
+                text("YOU PRESSED B", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>255 && mouseX <355 && mouseY>height-200 && mouseY <height-150){
+                fill(255);
+                text("YOU PRESSED A", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>465 && mouseX <565 && mouseY>height-200 && mouseY <height-150){
+                fill(255);
+                text("YOU PRESSED C", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>205 && mouseX <305 && mouseY>height-145 && mouseY <height-95){
+                fill(255);
+                text("YOU PRESSED D", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>310 && mouseX <410 && mouseY>height-145 && mouseY <height-95){
+                fill(255);
+                text("YOU PRESSED E", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>415 && mouseX <515 && mouseY>height-145 && mouseY <height-95){
+                fill(255);
+                text("YOU PRESSED F", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>520 && mouseX <620 && mouseY>height-145 && mouseY <height-95){
+                fill(255);
+                text("YOU PRESSED G", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>260 && mouseX <360 && mouseY>height-90 && mouseY <height-40){
+                fill(255);
+                text("YOU PRESSED H", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>365 && mouseX <465 && mouseY>height-90 && mouseY <height-40){
+                fill(255);
+                text("YOU PRESSED I", width/2, 175);
+            }
+        }
+        if(mousePressed){
+            if(mouseX>470 && mouseX <570 && mouseY>height-90 && mouseY <height-40){
+                fill(255);
+                text("YOU PRESSED J", width/2, 175);
             }
         }
     }
